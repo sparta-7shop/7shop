86805d9f5b2436468bc2bd1a0e1708aa5d8e3d3b
“Woo <wth2052@gmail.com>
2023-02-01 23:41:56 +0900

    add: router #4

---
+++ src/backend/routers/admin.routers.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+
+
+// //회원 삭제(관리자)
+// router.post('/admin/users/:userId', adminController.###);
+// //상품 수정(관리자)
+// router.put('/admin/product/:productId', adminController.###);
+// //상품 삭제(관리자)
+// router.post('/admin/product/:productId', adminController.###);
+// //상품 등록(관리자)
+// router.post('/admin/product', adminController.###);
+
+
+module.exports = router;
---
+++ src/backend/routers/product.routers.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+
+const productController = require('../controllers/product');
+
+const router = express.Router();
+
+// //상품 목록 불러오기
+// router.get('/products/list', productController.####)
+
+
+module.exports = router;
