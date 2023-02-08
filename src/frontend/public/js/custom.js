0f2b3c0434bdf8a4dc9431c50766ac3d867c5cee
“Woo TaeHyeon <wth2052@gmail.com>
2023-02-08 10:59:29 +0900

    🍡feat: shop/shop-category1/shop-category2 Pagination #32

--- src/frontend/public/js/custom.js
+++ src/frontend/public/js/custom.js
@@ -7,62 +7,6 @@ function showProduct() {
         get('/products/list')
         .then((res) => {
 
-            /* ------------------index 페이지 ------------------ */
-            const product1 = res.data.productList[0]
-            const product2 = res.data.productList[1]
-            const product3 = res.data.productList[2]
-            let temp_html1 = `
-                        <div class="row p-5">
-                            <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                                <img class="img-fluid" src=".//uploads/${product1.img_path}" alt="" />
-                            </div>
-                            <div class="col-lg-6 mb-0 d-flex align-items-center">
-                                <div class="text-align-left align-self-center">
-                                    <h1 class="h1 text-success"><b>7Shop</b> eCommerce</h1>
-                                    <h3 class="h2">${product1.name}</h3>
-                                    <p>
-                                        ${product1.description}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-            `
-            let temp_html2 = `
-                        <div class="row p-5">
-                            <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                                <img class="img-fluid" src=".//uploads/${product2.img_path}" alt="" />
-                            </div>
-                            <div class="col-lg-6 mb-0 d-flex align-items-center">
-                                <div class="text-align-left align-self-center">
-                                    <h1 class="h1 text-success"><b>7Shop</b> eCommerce</h1>
-                                    <h3 class="h2">${product2.name}</h3>
-                                    <p>
-                                        ${product2.description}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-            `
-            let temp_html3 = `
-                        <div class="row p-5">
-                            <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                                <img class="img-fluid" src=".//uploads/${product3.img_path}" alt="" />
-                            </div>
-                            <div class="col-lg-6 mb-0 d-flex align-items-center">
-                                <div class="text-align-left align-self-center">
-                                    <h1 class="h1 text-success"><b>7Shop</b> eCommerce</h1>
-                                    <h3 class="h2">${product3.name}</h3>
-                                    <p>
-                                        ${product3.description}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-            `
-            $('#container1').append(temp_html1);
-            $('#container2').append(temp_html2);
-            $('#container3').append(temp_html3);
-            /* ------------------shop 페이지 ------------------ */
             let rows = res.data.productList
             for (i in rows) {
                 let name = rows[i].name
@@ -72,7 +16,7 @@ function showProduct() {
                 let desc = rows[i].description
                 let productId = rows[i].id
                 let product = `
-                <div class="col-md-4">
+                <div class="col-md-4" id="paginate">
                 <div class="card mb-4 product-wap rounded-0">
                 <div class="card rounded-0">
                     <img
@@ -111,9 +55,7 @@ function showProduct() {
                 `
                 $('#product_list').append(product).trigger('create');
             }
-
-
-
+            createPaginate()
         })
         .catch((error) => {
             console.log(error);
